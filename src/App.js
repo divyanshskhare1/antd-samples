2845310b2e637c3b684c25f97b9a104adbde6631
Divyansh.Khare <Divyansh.Khare@PD3020-JN6F622.OpusConsulting.com>
2018-11-12 15:10:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+
+import Tables from './Pages/Table';
+import Layot from './Components/layout';
+     
+
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Main">
+     
+          <Layot/>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
