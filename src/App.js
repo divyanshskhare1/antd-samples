c901247bacc7d126965827dc50558529ec7f5756
Rahul.Mohan <Rahul.Mohan@PL440G4-725DJLJ.OpusConsulting.com>
2018-11-12 20:22:21 +0530

    Checking in the basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-
-import Tables from './Pages/Table';
-import Layot from './Components/layout';
-     
+import LandingPage from './layouts/landing-page';
 
 import './App.css';
-
-
 
 class App extends Component {
   render() {
     return (
       <div className="Main">
-     
-          <Layot/>
-          
+        <LandingPage />
       </div>
     );
   }
 }
-
 export default App;
